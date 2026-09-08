44e252b0d51add389914b202e97fa65ab7cc0dc0
Rich3art <richardaslabbert@gmail.com>
2026-09-08 21:52:02 +0400

    fix(installer): allow verified admin bootstrap

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ final class User extends Authenticatable implements MustVerifyEmail
         'avatar_path',
         'timezone',
         'password',
+        'email_verified_at',
         'two_factor_secret',
         'two_factor_recovery_codes',
         'two_factor_confirmed_at',
